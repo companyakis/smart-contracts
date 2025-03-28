27eaad7a71dc4b4eb06ee409c1f37282aa2aa186
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-28 21:26:25 +0300

    Create part-e-0001-variables3.rs

---
+++ part-e-0001-variables3.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x: i32;
+
+//     println!("Number {x}");
+// }
+
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = -21;
+
+    println!("Number {x}");
+}
