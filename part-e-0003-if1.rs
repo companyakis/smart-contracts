d6e4673bf560ab030d4f2b8b3418a32b0bbf011c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-31 18:11:15 +0300

    Create part-e-0003-if1.rs

---
+++ part-e-0003-if1.rs
@@ -0,0 +1,20 @@
+// fn bigger(a: i32, b: i32) -> i32 {
+//     // TODO: Complete this function to return the bigger number!
+//     // If both numbers are equal, any of them can be returned.
+//     // Do not use:
+//     // - another function call
+//     // - additional variables
+// }
+
+// fn main() {
+//     // You can optionally experiment here.
+// }
+
+fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
