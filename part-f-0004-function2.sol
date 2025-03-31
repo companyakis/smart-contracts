22c093743ff71c0608996cbf3a8e807a717153f6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-31 17:59:45 +0300

    Create part-f-0004-function2.sol

---
+++ part-f-0004-function2.sol
@@ -0,0 +1,26 @@
+/*Your Goal: Add Uint
+Create an external view function add which takes a uint parameter and returns the sum of the parameter plus the state variable x.*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint public x;
+
+    constructor(uint _x) {
+
+        x = _x;
+    }
+
+
+    function increment() external {
+
+        x += 1;
+    }
+
+    function add(uint _y) external view returns (uint) {
+
+        return x + _y;
+    }   
+}
