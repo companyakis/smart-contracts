182b4ca0aab5dc482c37e9d4d202b11122fe29ac
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-31 18:04:27 +0300

    Create part-f-0004-function4.sol

---
+++ part-f-0004-function4.sol
@@ -0,0 +1,22 @@
+/*
+Your Goal: Overload Double
+Create another pure external function double which takes two uint parameters.
+Double both of the arguments and return both of them in the same order they were passed into the function.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    function double(uint _a) external pure returns (uint) {
+
+        return _a * 2;
+    }
+
+    function double(uint _x, uint _y) external pure returns (uint, uint) {
+
+        return (_x * 2, _y * 2);
+    } 
+
+}
