fcd3202e76ca6be894606cded33914c987ea208a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-31 22:37:48 +0300

    Create part-e-0005-primitive_types1.rs

---
+++ part-e-0005-primitive_types1.rs
@@ -0,0 +1,34 @@
+// // Booleans (`bool`)
+
+// fn main() {
+//     let is_morning = true;
+//     if is_morning {
+//         println!("Good morning!");
+//     }
+
+//     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
+//     // The value of the variable should be the negation (opposite) of `is_morning`.
+//     // let …
+//     if is_evening {
+//         println!("Good evening!");
+//     }
+// }
+
+// Booleans (`bool`)
+
+fn main() {
+    let is_morning = true;
+
+    let is_evening: bool = !is_morning;
+
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
+    // The value of the variable should be the negation (opposite) of `is_morning`.
+    // let …
+    if is_evening {
+        println!("Good evening!");
+    }
+}
