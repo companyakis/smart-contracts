0d5e1e1d814a66ff584b6c61cbe558e0c530ef6c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-02 21:03:00 +0300

    Create part-f-0005-array1.sol

---
+++ part-f-0005-array1.sol
@@ -0,0 +1,22 @@
+/*Your Goal: Find the Sum
+Create a pure, external function sum which takes a fixed size array of five unsigned integers.
+Find the sum of the unsigned integers and return it as a uint.*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+
+    function sum(uint[5] memory x) external pure returns (uint) {
+
+        uint total = 0;
+
+        for (uint8 i = 0; i < 5; i++) {
+
+            total += x[i];
+        }
+
+        return total;
+    }
+    
+}
